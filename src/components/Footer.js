ff49b1e1df9841ab426050602b231a6108c11d2a
Tanisha-987 <coding.official441@gmail.com>
2026-02-17 15:29:18 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -85,27 +85,51 @@ const Footer = () => {
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-sm">
           <p className='text-left'>&copy; 2026 BITS International School. All rights reserved.</p>
-          <p className='text-right'>
-            Website Designed & Developed by:{' '}
-            <a
-              href="https://www.linkedin.com/in/aryansaini870/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-400 hover:text-blue-300 underline transition-colors duration-200"
-            >
-              Aryan
-            </a>
-            {' & '}
-            <a
-              href="https://www.linkedin.com/in/tanisha-garg-70116b313/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-400 hover:text-blue-300 underline transition-colors duration-200"
-            >
-              Tanisha
-            </a>
-          </p>
         </div>
+
+
+
+        <div className="mt-6 text-center animate-fadeInUp" style={{ animationDelay: '500ms' }}>
+            <p className="text-gray-400 text-sm space-x-2">
+              Designed and Developed by{' '}
+              <a
+                href="https://www.linkedin.com/in/aryansaini870/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mr-2 text-blue-300 hover:text-blue-200 font-semibold transition-all hover:underline decoration-2 underline-offset-4 inline-flex items-center space-x-2 group"
+              >
+                <span>Aryan</span>
+                <svg
+                  className="w-4 h-4 group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                </svg>
+              </a>
+
+              &
+
+              <a
+                href="https://www.linkedin.com/in/tanisha-garg-70116b313/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-300 hover:text-blue-200 font-semibold transition-all hover:underline decoration-2 underline-offset-4 inline-flex items-center space-x-2 group"
+              >
+                <span>Tanisha</span>
+                <svg
+                  className="w-4 h-4 group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                </svg>
+              </a>
+
+            </p>
+          </div>
       </div>
     </footer>
   );
