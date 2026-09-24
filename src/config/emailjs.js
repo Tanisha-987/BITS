00034268d6a379cc9aa355f6d28666ff540d9971
Aryan <107269876+pikachu71213@users.noreply.github.com>
2026-09-24 15:55:24 +0530

    saa

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -4,7 +4,7 @@
 
 export const emailjsConfig = {
   serviceId: process.env.REACT_APP_EMAILJS_SERVICE_ID || 'service_36ucllr',
-  templateId: process.env.REACT_APP_EMAILJS_TEMPLATE_ID || 'service_36ucllr',
+  templateId: process.env.REACT_APP_EMAILJS_TEMPLATE_ID || 'template_okszldp',
   publicKey: process.env.REACT_APP_EMAILJS_PUBLIC_KEY || 'O84XvhD_6Vbf5y5Uu',
 };
 
